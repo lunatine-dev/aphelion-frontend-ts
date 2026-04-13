c4b2a0f53a1070e527e4a6854935870071cef082
Lunatine <github@lunatine.dev>
2026-04-13 13:46:02 +0100

    feat: Error page + 404 logic

---
+++ src/routes/(dashboard)/+error.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { page } from "$app/state";
+
+    import * as Empty from "$lib/components/ui/empty";
+    import { Button } from "$lib/components/ui/button";
+</script>
+
+<Empty.Root class="w-full">
+    <Empty.Header>
+        <Empty.Title>{page.status}</Empty.Title>
+        <Empty.Description>{page?.error?.message ?? "Unknown Error Occurred"}</Empty.Description>
+        <Empty.Content>
+            <div class="flex gap-2">
+                <Button variant="outline" href="/">Go Back</Button>
+            </div>
+        </Empty.Content>
+    </Empty.Header>
+</Empty.Root>
