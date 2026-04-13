923ef809b5ea07389c802efcf3a619883c9a6441
Lunatine <github@lunatine.dev>
2026-04-13 14:04:18 +0100

    chore: Base page layout

--- src/routes/(dashboard)/apps/logs/+page.svelte
+++ src/routes/(dashboard)/apps/logs/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+    const PAGE_TITLE = $state("Logs");
+
     import Page from "$lib/components/blocks/content/Page.svelte";
 </script>
 
-<Page title="Dashboard">
+<Page title={PAGE_TITLE}>
     <div class="relative flex flex-col gap-4 overflow-auto px-4 lg:px-6">
-        <h1>Dashboard</h1>
+        <h1>{PAGE_TITLE}</h1>
     </div>
 </Page>
