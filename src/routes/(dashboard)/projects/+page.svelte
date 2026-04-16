f34a5beedf35fb9e584ec793a6cc7003b2a49461
Lunatine <github@lunatine.dev>
2026-04-16 17:57:08 +0100

    refactor: Rename applications to projects to be clear on the purpose

--- src/routes/(dashboard)/projects/+page.svelte
+++ src/routes/(dashboard)/projects/+page.svelte
@@ -1,16 +1,25 @@
 <script lang="ts">
     import { PROJECT_TEMPLATE } from "$lib/components/constants";
 
+    import { Button } from "$lib/components/ui/button";
+
     import Page from "$lib/components/blocks/content/Page.svelte";
     import ProjectItem from "./ProjectItem.svelte";
 
-    const PAGE_TITLE = $state("Applications");
+    import IconPlus from "@tabler/icons-svelte/icons/plus";
+
+    const PAGE_TITLE = $state("Projects");
     const repositories = Array.from({ length: 12 }, () => structuredClone(PROJECT_TEMPLATE));
 </script>
 
 <Page title={PAGE_TITLE}>
+    {#snippet buttonsSnippet()}
+        <Button>
+            <IconPlus />
+            New Project
+        </Button>
+    {/snippet}
     <div class="relative flex flex-col gap-4 overflow-auto px-4 lg:px-6">
-        <h1 class="font-heading">{PAGE_TITLE}</h1>
         <div
             class="grid grid-cols-1 gap-4 px-4 lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4 overflow-hidden py-1"
         >
