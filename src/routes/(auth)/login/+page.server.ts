a154a472bfbd2c66ab6908305317f1772f470af1
Lunatine <github@lunatine.dev>
2026-04-25 16:19:11 +0100

    refactor: Fix redirecting to wrong path

--- src/routes/(auth)/login/+page.server.ts
+++ src/routes/(auth)/login/+page.server.ts
@@ -4,7 +4,7 @@ import type { PageServerLoad } from "./$types";
 export const load: PageServerLoad = async ({ locals }) => {
     // If the hook successfully verified a JWT, redirect to home/dashboard
     if (locals.user) {
-        throw redirect(303, "/dashboard");
+        throw redirect(303, "/");
     }
 
     return {};
